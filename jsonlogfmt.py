5a870f8beef1cc0c5e0fc8d8f446882ab904fc07
Constantine Kormashev <constantine.kormashev@gmail.com>
2018-02-06 12:05:12 +0300

    license added

--- jsonlogfmt.py
+++ jsonlogfmt.py
@@ -1,3 +1,16 @@
+# Copyright 2018 by Constantine Kormashev. All Rights Reserved.
+# Licensed under Mozilla Public License Version 2.0 (the "License");
+# You may obtain a copy of the License at
+# https://www.mozilla.org/en-US/MPL/2.0/
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+#
 # Formatting class for logging.Logger, composes JSON message from logging.Logger message.
 # Using is pretty simple, just use JSONMapFormatter obj in `setFormatter` of logging.Handler obj
 
